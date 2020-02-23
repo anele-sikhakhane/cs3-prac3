57eb725d79cef9d796b28b33d40c637b3e57f0c9
Anele Sikhakhane <G18s6106@ict.ru.ac.za>
2020-02-23 22:41:29 +0200

    hope this works?

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,61 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            static string Prefix(string input)
+            {
+            int words = 0;
+            string wordx = "";
+            string words1 = "";
+            string inputx = input;
+            int len = input.Length;
+            string lenx = len.ToString();
+
+            //Trim whitespace from beginning and end of string
+            input = input.Trim();
+
+            //Necessary because foreach will einputecute once with empty string returning 1
+           if (input == "")
+           {
+                Console.WriteLine("0,0:");
+                return "0,0:";
+           }
+
+            
+           while (input.Contains("  "))
+               input = input.Replace("  ", " ");
+
+            while (input.Contains("."))
+                input = input.Replace(".","");
+                       
+            while (input.Contains("?"))
+                input = input.Replace("?","");
+
+
+           foreach (string y in input.Split(' '))
+               words++;
+            
+            wordx = words.ToString();
+            Console.WriteLine(input);
+            words1 = (lenx + "," + wordx + ":" + inputx);
+            Console.WriteLine(words1);
+
+            return words1;
+                
+             
+
+            }
+
+        // Prefix("   anele   ");
+            Prefix("Hello");
+            Prefix("");
+            Prefix("what ... did you say?? ");
+
+            
         }
+
     }
 }
