8e64f3d12325d58750a1e8cbf2fec9c7c4084ab1
Anele Sikhakhane <G18s6106@ict.ru.ac.za>
2020-02-24 20:11:29 +0200

    oof3

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+//using Program.cs;
+using Problems;
 
 namespace Problems.Tests
 {
@@ -11,8 +13,27 @@ namespace Problems.Tests
 
         [Test]
         public void Test1()
-        {
-            Assert.Pass();
+        {   
+            Program px = new Program();
+            string ans1 = px.Prefix("hello");
+            Assert.That(ans1, Is.EqualTo("5,1:hello"));
         }
+        [Test]
+
+        public void Test2()
+        {
+            Program px1 = new Program();
+            string ans2 = px1.Prefix("");
+            Assert.That(ans2, Is.EqualTo( "0,0:"));
+        }
+        [Test]
+        public void Test3()
+        {
+            Program px2 = new Program();
+            string ans3 = px2.Prefix("what ... did you say?? ");
+            Assert.That(ans3, Is.EqualTo("27,5:what ... did you say?? "));
+
+        }
+
     }
 }
